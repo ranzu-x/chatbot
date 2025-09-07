7fdadbd831507357c83e47807e618a7f8bb0b617
ranzu-x <ranzu84@gmail.com>
2025-09-08 00:09:02 +0600

    first commit

---
+++ hms_frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import router from './Router/router.jsx'
+import { RouterProvider } from 'react-router'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>
+)
