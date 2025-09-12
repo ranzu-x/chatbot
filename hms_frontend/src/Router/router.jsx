008b75a569fcd8f722e188eb377703f8dc8d07b6
Toufik laptop <toufikhasan088@gmail.com>
2025-09-13 01:45:56 +0600

    aaaa

--- hms_frontend/src/Router/router.jsx
+++ hms_frontend/src/Router/router.jsx
@@ -1,21 +1,13 @@
 import { createBrowserRouter } from "react-router";
-<<<<<<< HEAD
-=======
 import HomeLayout from "../Layout/HomeLayout";
->>>>>>> 981aff40e508eaf7b7f7d62501fc600894f06dee
 import Home from "../Pages/Home/Home";
 import LogIn from "../Pages/LogIn/LogIn";
 import Register from "../Pages/Register/Register";
-import HomeLayout from "../Layout/HomeLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-<<<<<<< HEAD
-    element: <HomeLayout />,
-=======
     Component: HomeLayout,
->>>>>>> 981aff40e508eaf7b7f7d62501fc600894f06dee
     children: [
       {
         path: "/",
