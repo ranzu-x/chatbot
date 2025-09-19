6da5f7fea62dc50ed127dd0311d8b42464fde0cd
Toufik laptop <toufikhasan088@gmail.com>
2025-09-20 01:12:48 +0600

    dashbord route change

--- hms_frontend/src/Provider/AuthContexProvider.jsx
+++ hms_frontend/src/Provider/AuthContexProvider.jsx
@@ -7,7 +7,7 @@ export const AuthContexProvider = ({ children }) => {
 
 
   const login = async (email, password) => {
-    const res = await fetch("http://localhost:5000/api/v1/superadmin/login", {
+    const res = await fetch("http://192.168.0.100:5000/api/v1/superadmin/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
