f5c57341172c50984033e9ab651fc248aa3960c4
Toufik laptop <toufikhasan088@gmail.com>
2025-10-07 23:14:27 +0600

    add profile on dashboard

--- hms_frontend/src/Layout/HomeLayout.jsx
+++ hms_frontend/src/Layout/HomeLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../Components/Common/NavBar';
 import { Outlet } from 'react-router';
+import TestNav from '../Components/Common/TestNav';
 
 const HomeLayout = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <TestNav></TestNav>
             <Outlet></Outlet>
         </div>
     );
